e2012ea6a345935813e5512cac92d8c85bcb53fa
ischiuxd <58531991+ischiu@users.noreply.github.com>
2024-11-26 20:25:18 -0600

    onMount

--- src/components/contador.svelte
+++ src/components/contador.svelte
@@ -1,5 +1,8 @@
 <script>
+	import { onMount } from 'svelte';
+
 	let count = 0;
+
 	function increment() {
 		count += 1;
 	}
@@ -11,16 +14,16 @@
 	function reset() {
 		count = 0;
 	}
+
+	onMount(() => {
+		console.log('El componente ha sido montado');
+		count = 10;
+	});
 </script>
 
 <div class="grid place-items-center gap-4">
 	<h1 class="text-secondary">Contador: {count}</h1>
 	<button class="p-2 bg-tertiary text-white rounded-lg" on:click={increment}>Incrementar</button>
-    <button class="p-2 bg-tertiary text-white rounded-lg" on:click={reset}>Reiniciar</button>
+	<button class="p-2 bg-tertiary text-white rounded-lg" on:click={reset}>Reiniciar</button>
 	<button class="p-2 bg-tertiary text-white rounded-lg" on:click={decrement}>Decrementar</button>
-
 </div>
-
-<style>
-
-</style>
